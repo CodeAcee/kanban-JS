075b1f96c6cb9e63b354801c2a6f801bccc035a2
yaroslav_p <yaroslav6066@gmail.com>
2019-11-22 13:45:28 +0200

    add data render

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-const app = document.getElementById('#app')
+import service from '../src/service';
+
+const app = document.querySelector('#app');
 app.classList.add('#app');
 
 const board = document.createElement('div');
 board.classList.add('board');
 app.appendChild(board);
+
+
+// const btn = document.createElement('button');
+// btn.textContent = "+";
+// btn.classList.add("header__btn");
+// header.appendChild(btn);
+
+
+// const columnArea = document.createElement('div');
+// columnArea.classList.add('column__block');
+// app.appendChild(columnArea);
+
+
+// const generateColumn = () => {
+//   const column = document.createElement('div');
+//   column.classList.add('column');
+//   columnArea.appendChild(column);
+//   return column;
+// }
+
+// btn.addEventListener('click', generateColumn);
+
