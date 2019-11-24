d4a54ec250e7058c3bc5872948c7c5355f2a740a
yaroslav_p <yaroslav6066@gmail.com>
2019-11-24 14:49:55 +0200

    fixed data json

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import card from "../src/services/card";
 import column from "../src/services/column";
 
 const app = document.querySelector("#app");
-app.classList.add("#app");
-
 const board = document.createElement('div');
+board.classList.add('column__block');
 app.appendChild(board);
