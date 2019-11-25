00046e96950a2afe69f369e3910ee047e3629a03
yaroslav_p <yaroslav6066@gmail.com>
2019-11-25 11:06:50 +0200

    merge card branch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { getCards } from "../src/services/card";
 import { getColums } from "../src/services/column";
 import { generateColumn } from '../src/render/render_column';
-
+// import { generateCards } from '../src/render/render__card';
 
 const app = document.querySelector("#app");
 const board = document.createElement("div");
--- src/render/render_column.js
+++ src/render/render_column.js
@@ -10,3 +10,4 @@ export const generateColumn = (column) => {
 
   return columnEl;
 };
+
