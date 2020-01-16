6c82c8c891d5b4c065a6c3e4a8b0a8e3bf12df9f
yaroslav_p <yaroslav6066@gmail.com>
2020-01-16 10:35:19 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 import { getCards } from "../src/services/card";
 import { getColums } from "../src/services/column";
-import { renderColumn } from '../src/render/render_column';
-import { renderCard } from '../src/render/render_card';
+import { renderColumn } from "../src/render/render_column";
+import { renderCard } from "../src/render/render_card";
 
-const app = document.getElementById('app');
-const board = document.createElement('div');
-board.classList.add('column__block');
+const app = document.getElementById("app");
+const board = document.createElement("div");
+board.classList.add("column__block");
 app.appendChild(board);
 
 const columns = getColums();
 const cards = getCards();
 
-columns.forEach((column) => {
+columns.forEach(column => {
   const columnEl = renderColumn(column);
-  cards.filter((card) => card.colId === column.id)
-        .map(renderCard)
-        .forEach(card => columnEl.append(card))  
+  cards
+    .filter(card => card.colId === column.id)
+    .map(renderCard)
+    .forEach(card => columnEl.append(card));
   board.append(columnEl);
-})
-
+});
--- src/render/render_column.js
+++ src/render/render_column.js
@@ -1,15 +1,12 @@
-export const renderColumn = (column) => {
-  const columnEl = document.createElement('div');
-  columnEl.classList.add('column');
-  columnEl.setAttribute('column-id', column.id);
+export const renderColumn = column => {
+  const columnEl = document.createElement("div");
+  columnEl.classList.add("column");
+  columnEl.setAttribute("column-id", column.id);
 
-  const columnName = document.createElement('div');
+  const columnName = document.createElement("div");
   columnName.innerHTML = column.name;
 
   columnEl.appendChild(columnName);
 
   return columnEl;
 };
-
-
-
